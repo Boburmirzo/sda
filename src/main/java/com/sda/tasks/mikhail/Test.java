8f68508b427312e087305d29d2d13b7b74a85c3e
mikhailshepelev <romanist-mike@yandex.ru>
2019-11-10 14:25:49 +0200

    optimized code

--- src/main/java/com/sda/tasks/mikhail/Test.java
+++ src/main/java/com/sda/tasks/mikhail/Test.java
@@ -2,13 +2,15 @@ package com.sda.tasks.mikhail;
 
 public class Test {
     public boolean hasTeen(int a, int b, int c) {
-        int [] arr = new int [3];
-        arr[0] = a;
-        arr[1] = b;
-        arr[2] = c;
+        int [] arr = {a, b, c};
         for (int i = 0; i<arr.length; i++){
             if (arr[i]>=13 && arr[i]<=19) return true;
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        System.out.println(t.hasTeen(15, 17, 20));
     }
 }
