2bbfc8c4218add778d5fa724b5a2043a6e16d17b
Ilya Garkusha <ilja.garkusha94@gmail.com>
2019-11-10 14:26:53 +0200

    deleted code

--- src/main/java/com/sda/tasks/ilya/Front3.java
+++ src/main/java/com/sda/tasks/ilya/Front3.java
@@ -1,16 +1,5 @@
 package com.sda.tasks.ilya;
 
 public class Front3 {
-    public String front3(String str) {
-        String front;
-
-        if (str.length() >= 3) {
-            front = str.substring(0, 3);
-        }
-        else {
-            front = str;
-        }
-
-        return front + front + front;
-    }
+    // delete code
 }
