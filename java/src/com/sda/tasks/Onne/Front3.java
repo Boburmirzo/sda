07d09f7e95b7fc5f1fc06e59b268fb388423741f
Onne <summaarum@gmail.com>
2019-11-10 18:03:36 +0200

    Test.

---
+++ java/src/com/sda/tasks/Onne/Front3.java
@@ -0,0 +1,16 @@
+package com.sda.tasks.Onne;
+
+    public class Front3 {
+        public String  front3(String str) {
+            String front;
+
+            if (str.length() >= 3) {
+                front = str.substring(0, 3);
+            }
+            else {
+                front = str;
+            }
+            return front + front + front;
+        }
+    }
+
